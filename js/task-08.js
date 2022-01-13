f86717ec19b318fe4718ed76aedb3d1ccd8a761b
Anton-Yasenovych <87947933+Anton-Yasenovych@users.noreply.github.com>
2022-01-14 00:55:10 +0200

    point_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const getFormElem = document.querySelector('form.login-form');
+getFormElem.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {email, password} = event.currentTarget;
+    
+     if (email.value === '' || password.value === '') {
+        return alert('ATTENTION!!! Please fill in all the fields!');
+      };
+
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
 
