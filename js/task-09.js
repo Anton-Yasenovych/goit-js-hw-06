0735948cbe083f93308b32f2d02a4cb8bd9bc3ef
Anton-Yasenovych <87947933+Anton-Yasenovych@users.noreply.github.com>
2022-01-14 02:35:30 +0200

    saretyu

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const getBtnElem = document.querySelector('button.change-color');
+getBtnElem.addEventListener('click', handlerSubmit);
+
+function handlerSubmit(event){
+
+//  
+return getSpanElem = getRandomHexColor;
+console.log(getSpanElem);
+}
